70da59122895419ce1651d94f83e80912fa4c384
Billy <billyjcatchpole@gmail.com>
2020-09-21 15:55:49 +0100

    Added nginx routing, skaffold and a dummy endpoint in the auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,4 +5,9 @@ const app = express()
 
 app.use(json())
 
+app.get("/api/users/currentuser", (_,res) => {
+
+    res.status(200).send("Its working")
+})
+
 app.listen(3000, () => { console.log("Auth running on 3000") })
