50106c888a6edb8c80767b0e179ab2adb34191e3
Billy <billyjcatchpole@gmail.com>
2020-09-22 09:58:44 +0100

    Added standardised error handling so the frontend can work with it easier

--- auth/src/middleware/error-handler.ts
+++ auth/src/middleware/error-handler.ts
@@ -5,15 +5,24 @@ import { DatabaseConnectionError } from "../errors/database-connection-error"
 
 const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
 
-    if(err instanceof RequestValidationError){//if it is a req vali error
-        console.log("handling as req vali error")
+    if (err instanceof RequestValidationError) {//if it is a req vali error
+
+        const formatted_errors = err.errors.map(error => {
+
+            return { message: error.msg, field: error.param }
+
+        })
+
+        return res.status(424).send({ errors: formatted_errors })
     }
 
-    if(err instanceof DatabaseConnectionError){//if it is a db conni error
-        console.log("handling as db conni error")
+    if (err instanceof DatabaseConnectionError) {//if it is a db conni error
+
+        return res.status(500).send({ errors: [{ message: err.reason }] })
+
     }
 
-    res.status(400).send(err.message)
+    res.status(400).send({errors:[{message: "Something went wrong"}]})
 
 }
 
